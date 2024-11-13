904a3efe1d39c1758070ae7c1db9a8a44758cf7b
SamarthGautam1 <samartgautamreal@gmail.com>
2024-11-14 00:44:04 +0530

    Code

---
+++ LinkedList/moreOnLL/findloops.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+bool hasCycle(struct Node* head) {
+    struct Node *slow = head, *fast = head;
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = createNode(1);
+    head->next = createNode(2);
+    head->next->next = createNode(3);
+    head->next->next->next = createNode(4);
+    head->next->next->next->next = createNode(5);
+
+    // head->next->next->next->next->next = head->next;
+
+    if (hasCycle(head)) {
+        printf("Cycle detected in the linked list.\n");
+    } else {
+        printf("No cycle detected in the linked list.\n");
+    }
+
+    return 0;
+}
